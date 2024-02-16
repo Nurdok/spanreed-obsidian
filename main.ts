319f896593694c11808627890c793f8006323cbf
Amir Rachum <amir@rachum.com>
2024-02-16 14:45:39 +0200

    Remove some redundant code.

--- main.ts
+++ main.ts
@@ -56,42 +56,6 @@ export default class SpanreedPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				this.app.commands.executeCommandById("markdown:add-metadata-property");
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SpanreedSettingsTab(this.app, this));
 
@@ -239,31 +203,6 @@ export default class SpanreedPlugin extends Plugin {
 		console.log("done polling redis task message queue")
 		this.registerInterval(window.setTimeout(() => this.pollRedisTaskMessageQueue(), 10 * 1000));
 	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	async onOpen() {
-		const {contentEl} = this;
-		contentEl.createEl('h1', {text: 'Book Query'});
-		const dv = this.app.plugins.plugins.dataview.api;
-		console.log("before query");
-		dv.tryQuery("table without id title from #book").then((res) => {
-			contentEl.createEl('h2', {text: 'Your books'});
-			res.values.forEach((book) => {
-				contentEl.createEl('p', {text: book[0]});
-			});
-		});
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-
 }
 
 class SpanreedSettingsTab extends PluginSettingTab {
