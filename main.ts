710edc5b76d40e04226172bb7aa5e8141f730b14
Amir Rachum <amir@rachum.com>
2024-04-20 14:22:37 +0300

    Handle multiple envs (+refactor).

--- main.ts
+++ main.ts
@@ -27,19 +27,42 @@ interface SpanreedRpcResponse {
 interface QueryDataviewResult {
 }
 
-interface SpanreedSettings {
+interface ConnectionSettings {
 	spanreedUserId: number;
-	redis_url: string;
+	redisUrl: string;
+}
+
+type Environment = 'production' | 'staging';
+
+const toString = (env: Environment) => {
+	return {
+		'production': 'Production',
+		'staging': 'Staging',
+	}[env];
+}
+
+interface SpanreedSettings {
+	connectionSettings: Record<Environment, ConnectionSettings>
+	activeEnvironment: Environment
+}
+
+const DEFAULT_CONNECTION_SETTINGS: ConnectionSettings = {
+	spanreedUserId: -1,
+	redisUrl: "",
 }
 
 const DEFAULT_SETTINGS: SpanreedSettings = {
-	spanreedUserId: -1,
-	redis_url: "",
+	connectionSettings: {
+		production: DEFAULT_CONNECTION_SETTINGS,
+		staging: DEFAULT_CONNECTION_SETTINGS,
+	},
+	activeEnvironment: 'production',
 }
 
 export default class SpanreedPlugin extends Plugin {
 	settings: SpanreedSettings;
 	redisClient: RedisClientType<any, any, any>;
+	lastUsedRedisUrl?: string
 
 	async onload() {
 		await this.loadSettings();
@@ -59,20 +82,16 @@ export default class SpanreedPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SpanreedSettingsTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setTimeout(() => this.pollRedisTaskMessageQueue(), 0));
 
-		if (this.settings.spanreedUserId === -1) {
+		const connectionSettings = this.getActiveConnectionSettings()
+
+		if (connectionSettings.spanreedUserId === -1) {
 			new Notice("Please set your Spanreed user ID in the plugin settings.");
 			return;
 		}
-		if (this.settings.redis_url === "") {
+		if (connectionSettings.redisUrl === "") {
 			new Notice("Please set your Redis URL in the plugin settings.");
 			return;
 		}
@@ -90,119 +109,149 @@ export default class SpanreedPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	async pollRedisTaskMessageQueue() {
-		if (typeof (this.redisClient) === "undefined") {
-			this.redisClient = createClient({
-				url: this.settings.redis_url,
-			});
-			await this.redisClient.connect();
-		}
+	getActiveConnectionSettings(): ConnectionSettings {
+		return this.settings.connectionSettings[this.settings.activeEnvironment];
+	}
 
-		await this.redisClient.blPop(`obsidian-plugin-tasks:${this.settings.spanreedUserId}`, 0)
-			.then(async (res) => {
-				if (res === null) {
-					return;
+	async handleSpanreedRequest(request: SpanreedRpcRequest): Promise<SpanreedRpcResponse> {
+		let response: SpanreedRpcResponse = {"success": false, "result": "unknown error"};
+		switch (request.method) {
+			case "generate-daily-note": {
+				console.log("generating daily note")
+				this.app.commands.executeCommandById("daily-notes");
+				response = {"success": true, "result": null};
+				break;
+			}
+			case "modify-property": {
+				let filepath: string = request.params.filepath;
+				let tfile: TFile | null = null;
+				for (let file of this.app.vault.getMarkdownFiles()) {
+					if (file.path == filepath) {
+						tfile = file;
+						break;
+					}
 				}
-				let request: SpanreedRpcRequest = JSON.parse(res.element);
-				let responseQueue = `obsidian-plugin-tasks:${this.settings.spanreedUserId}:${request.request_id}`;
-				let response: SpanreedRpcResponse = {"success": false, "result": "unknown error"};
-
-				console.log("got request", request)
-				switch (request.method) {
-					case "generate-daily-note": {
-						console.log("generating daily note")
-						this.app.commands.executeCommandById("daily-notes");
-						response = {"success": true, "result": null};
-						break;
-					}
-					case "modify-property": {
-						let filepath: string = request.params.filepath;
-						let tfile: TFile | null = null;
-						for (let file of this.app.vault.getMarkdownFiles()) {
-							if (file.path == filepath) {
-								tfile = file;
-								break;
+				if (tfile === null) {
+					response = {"success": false, "result": "file not found"};
+					break;
+				}
+				const params: ModifyPropertyParams = request.params;
+				const property = params.property;
+				switch (params.operation) {
+					case "addToList":
+						await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
+							if (typeof (frontmatter[property]) === "undefined") {
+								frontmatter[property] = [];
 							}
-						}
-						if (tfile === null) {
-							response = {"success": false, "result": "file not found"};
-							break;
-						}
-						const params: ModifyPropertyParams = request.params;
-						const property = params.property;
-						switch (params.operation) {
-							case "addToList":
-								await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
-									if (typeof (frontmatter[property]) === "undefined") {
-										frontmatter[property] = [];
-									}
-									if (!Array.isArray(frontmatter[property])) {
-										response = {"success": false, "result": "property is not a list"};
-										return;
-									}
-									if (frontmatter[property].indexOf(params.value) <= -1) {
-										frontmatter[property].push(params.value);
-									}
-									response = {"success": true, result: null};
-								});
-								break;
-							case "removeFromList":
-								await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
-									if (typeof (frontmatter[property]) === "undefined") {
-										return;
-									}
-									if (!Array.isArray(frontmatter[property])) {
-										response = {"success": false, "result": "property is not a list"};
-										return;
-									}
-									let index = frontmatter[property].indexOf(params.value);
-									if (index > -1) {
-										frontmatter[property].splice(index, 1);
-									}
-									response = {"success": true, result: null};
-								});
-								break;
-							case "setSingleValue":
-								await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
-									frontmatter[property] = params.value;
-								});
-								response = {"success": true, result: null};
-								break;
-							case "deleteProperty":
-								await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
-									delete frontmatter[property];
-								});
-								response = {"success": true, result: null};
-								break;
-							case "getProperty":
-								// TODO: there's a better API for this, but I CBA right now
-								await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
-									let value = frontmatter[property];
-									if (typeof (value) === "undefined") {
-										value = null;
-									}
-									response = {"success": true, result: value}
-								});
-								break;
-						}
-						break;
-					}
-					case "query-dataview": {
-						const dv = this.app.plugins.plugins.dataview.api;
-						const params: QueryDataviewParams = request.params;
-						const query = params.query;
-						await dv.tryQuery(query).then((result: any) => {
-							response = {"success": true, "result": result};
+							if (!Array.isArray(frontmatter[property])) {
+								response = {"success": false, "result": "property is not a list"};
+								return;
+							}
+							if (frontmatter[property].indexOf(params.value) <= -1) {
+								frontmatter[property].push(params.value);
+							}
+							response = {"success": true, result: null};
 						});
 						break;
-					}
-					default:
-						response = {"success": false, "result": `unknown method ${request.method}`};
+					case "removeFromList":
+						await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
+							if (typeof (frontmatter[property]) === "undefined") {
+								return;
+							}
+							if (!Array.isArray(frontmatter[property])) {
+								response = {"success": false, "result": "property is not a list"};
+								return;
+							}
+							let index = frontmatter[property].indexOf(params.value);
+							if (index > -1) {
+								frontmatter[property].splice(index, 1);
+							}
+							response = {"success": true, result: null};
+						});
+						break;
+					case "setSingleValue":
+						await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
+							frontmatter[property] = params.value;
+						});
+						response = {"success": true, result: null};
+						break;
+					case "deleteProperty":
+						await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
+							delete frontmatter[property];
+						});
+						response = {"success": true, result: null};
+						break;
+					case "getProperty":
+						// TODO: there's a better API for this, but I CBA right now
+						await this.app.fileManager.processFrontMatter(tfile, (frontmatter) => {
+							let value = frontmatter[property];
+							if (typeof (value) === "undefined") {
+								value = null;
+							}
+							response = {"success": true, result: value}
+						});
+						break;
 				}
-				await this.redisClient.lPush(responseQueue, JSON.stringify(response));
-			});
-		console.log("done polling redis task message queue")
-		this.registerInterval(window.setTimeout(() => this.pollRedisTaskMessageQueue(), 10 * 1000));
+				break;
+			}
+			case "query-dataview": {
+				const dv = this.app.plugins.plugins.dataview.api;
+				const params: QueryDataviewParams = request.params;
+				const query = params.query;
+				await dv.tryQuery(query).then((result: any) => {
+					response = {"success": true, "result": result};
+				});
+				break;
+			}
+			default:
+				response = {"success": false, "result": `unknown method ${request.method}`};
+		}
+		return response
+	}
+
+	async ensureRedisClient() {
+		const activeConnectionSettings = this.getActiveConnectionSettings()
+		if (this.lastUsedRedisUrl === undefined ||
+			(activeConnectionSettings.redisUrl !== this.lastUsedRedisUrl)) {
+				if (this.redisClient !== undefined) {
+					await this.redisClient.quit();
+				}
+				this.redisClient = createClient({
+					url: activeConnectionSettings.redisUrl,
+				});
+				await this.redisClient.connect();
+				this.lastUsedRedisUrl = activeConnectionSettings.redisUrl
+		}
+	}
+
+	async pollRedisTaskMessageQueue() {
+		try {
+			await this.ensureRedisClient()
+			console.log("polling redis task message queue")
+
+			const spanreedUserId = this.getActiveConnectionSettings().spanreedUserId;
+			const taskQueue = `obsidian-plugin-tasks:${spanreedUserId}`;
+
+			console.log("Waiting on queue", taskQueue)
+
+			await this.redisClient.blPop(taskQueue, 60 /* timeout, in seconds */)
+				.then(async (res) => {
+					if (res === null) {
+						return;
+					}
+					let request: SpanreedRpcRequest = JSON.parse(res.element);
+					console.log("got request", request)
+					let response: SpanreedRpcResponse = await this.handleSpanreedRequest(request);
+					console.log("sending response", response)
+					let responseQueue = `obsidian-plugin-tasks:${spanreedUserId}:${request.request_id}`;
+					await this.redisClient.lPush(responseQueue, JSON.stringify(response));
+				});
+			console.log("done polling redis task message queue")
+		} catch (e) {
+			console.log("error polling redis task message queue", e)
+		} finally {
+			this.registerInterval(window.setTimeout(() => this.pollRedisTaskMessageQueue(), 10 * 1000));
+		}
 	}
 }
 
@@ -219,26 +268,50 @@ class SpanreedSettingsTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', {text: 'Environment'});
+
 		new Setting(containerEl)
-			.setName('Spanreed User ID')
-			.setDesc('Your Spanreed user ID')
-			.addText(text => text
-				.setPlaceholder('Enter your Spanreed user ID')
-				.setValue(this.plugin.settings.spanreedUserId.toString())
+			.setName('Active Environment')
+			.setDesc('The environment to use for Spanreed')
+			.addDropdown(dropdown => dropdown
+				.addOptions({
+					'production': 'Production',
+					'staging': 'Staging',
+				})
+				.setValue(this.plugin.settings.activeEnvironment)
 				.onChange(async (value) => {
-					this.plugin.settings.spanreedUserId = parseInt(value);
+					this.plugin.settings.activeEnvironment = value as Environment;
 					await this.plugin.saveSettings();
 				}));
 
-		new Setting(containerEl)
-			.setName('Redis URL')
-			.setDesc('Your Redis URL')
-			.addText(text => text
-				.setPlaceholder('Enter your Redis URL')
-				.setValue(this.plugin.settings.redis_url)
-				.onChange(async (value) => {
-					this.plugin.settings.redis_url = value;
-					await this.plugin.saveSettings();
-				}));
+		containerEl.createEl('h3', {text: 'Connection Settings'});
+
+		for (let env in this.plugin.settings.connectionSettings) {
+			containerEl.createEl('h4', {text: toString(env as Environment)});
+			let connectionSettings = this.plugin.settings.connectionSettings[env as Environment]
+
+			new Setting(containerEl)
+				.setName(`Spanreed User ID`)
+				.setDesc('Your Spanreed user ID')
+				.addText(text => text
+					.setPlaceholder('Enter your Spanreed user ID')
+					.setValue(connectionSettings.spanreedUserId.toString())
+					.onChange(async (value) => {
+						connectionSettings.spanreedUserId = parseInt(value);
+						await this.plugin.saveSettings();
+					}));
+
+			new Setting(containerEl)
+				.setName('Redis URL')
+				.setDesc('Your Redis URL')
+				.addText(text => text
+					.setPlaceholder('Enter your Redis URL')
+					.setValue(connectionSettings.redisUrl)
+					.onChange(async (value) => {
+						connectionSettings.redisUrl = value;
+						await this.plugin.saveSettings();
+					}));
+		}
+
 	}
 }
